0344e15c7a81df9291aaf780ba0ec29f5674939a
47tcyrgstj-design <47tcyrgstj@privaterelay.appleid.com>
2026-08-23 15:37:35 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1789,10 +1789,11 @@ const styles = StyleSheet.create({
     textAlign: "center",
   },
 
-  managerWelcomeText: {
+    managerWelcomeText: {
     color: "#aaa",
     fontSize: 14,
     lineHeight: 23,
     marginTop: 8,
     textAlign: "center",
   },
+});
