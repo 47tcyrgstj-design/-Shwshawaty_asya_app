69aba02ae60a8697ee0cf53b4e5096d899c363c2
47tcyrgstj-design <47tcyrgstj@privaterelay.appleid.com>
2026-08-23 23:38:28 +0300

    Update App.js

--- App.js
+++ App.js
@@ -432,7 +432,11 @@ export default function App() {
 const [customersLoading, setCustomersLoading] = useState(true);
   const [productsLoading, setProductsLoading] = useState(true);
   const [productsError, setProductsError] = useState("");
-
+  
+const [customerName, setCustomerName] = useState("");
+const [customerPhone, setCustomerPhone] = useState("");
+const [productName, setProductName] = useState("");
+const [customerTotal, setCustomerTotal] = useState("");
   /* =========================
      FIRESTORE PRODUCTS
   ========================= */
